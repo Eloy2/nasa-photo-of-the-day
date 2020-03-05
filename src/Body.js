b69424a91d83fbb49ae2b4ed0f6b1e87fbc5ce24
Eloy <eloydgutierrez@gmail.com>
2020-03-05 15:49:33 -0500

    added code to meet MVP for styled components

--- src/Body.js
+++ src/Body.js
@@ -1,15 +1,43 @@
 import React from "react";
 import "./App.css";
+import styled from 'styled-components';
+
+////////////////////////////////////STYLES//////////////////////////////
+const BodyStyle = styled.div`
+    background-color: #3a4b6b;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    height: 50%;
+`;
+
+const TextStyle = styled.p`
+    color: whitesmoke;
+    width: 36%;
+    background-color: #282c34;
+    border: 1px solid whitesmoke;
+    border-radius: 20px;
+    padding: 2%;
+`;
+
+const ImageStyle = styled.img`
+    margin-top: 2%;
+    width: 40%;
+    border: 1px solid whitesmoke;
+    border-radius: 20px;
+`;
+////////////////////////////////////STYLES//////////////////////////////
 
 function Body(props) {
     return (
-        <div className="Body">
-            <img
+        <BodyStyle>
+            <ImageStyle
             alt="space"
             src={props.imageUrl}
             />
-            <p>{props.Text}</p>
-        </div>
+            <TextStyle>{props.Text}</TextStyle>
+        </BodyStyle>
     );
 }
 
--- src/Head.js
+++ src/Head.js
@@ -1,12 +1,26 @@
 import React from "react";
 import "./App.css";
+import styled from 'styled-components';
+
+////////////////////////////////////STYLES//////////////////////////////
+const HeadStyle = styled.div`
+    background-color: #282c34;
+    min-height: 20vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+`;
+////////////////////////////////////STYLES//////////////////////////////
 
 function Head(props) {
     return (
-        <div className="App-header">
+        <HeadStyle>
             <h1>{props.Title}</h1>
             <h3>{props.Date}</h3>
-        </div>
+        </HeadStyle>
     );
 }
 
