78e3b06443eb7a78038d9f1b464b2b83cd5a76bf
Eloy <eloydgutierrez@gmail.com>
2020-03-05 15:54:07 -0500

    added code to meet MVP for styled components

--- src/Head.js
+++ src/Head.js
@@ -12,6 +12,7 @@ const HeadStyle = styled.div`
     justify-content: center;
     font-size: calc(10px + 2vmin);
     color: white;
+    border-bottom: 1px solid whitesmoke;
 `;
 ////////////////////////////////////STYLES//////////////////////////////
 
